2515cf72bd9339d07d88e9c50cc1e8db4c4a4967
matig <matig@DESKTOP-393FU0Q>
2020-05-19 22:57:51 -0400

    Confirmar

---
+++ SystemYuyitos/YuyitosLibrary/EstadoRecepcion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuyitosLibrary
+{
+    class EstadoRecepcion
+    {
+        private int _id_estado_recepcion;
+        private string _nombre_estado;
+
+        public int Id_estado_recepcion
+        {
+            get
+            {
+                return _id_estado_recepcion;
+            }
+
+            set
+            {
+                _id_estado_recepcion = value;
+            }
+        }
+
+        public string Nombre_estado
+        {
+            get
+            {
+                return _nombre_estado;
+            }
+
+            set
+            {
+                _nombre_estado = value;
+            }
+        }
+    }
+}
---
+++ SystemYuyitos/YuyitosLibrary/RecepcionCompra.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuyitosLibrary
+{
+    class RecepcionCompra
+    {
+        private string _id_recepcion_compra;
+        private DateTime _fecha;
+        private string _comentarios;
+        private EstadoRecepcion _estado_recepcion;
+        private int _id_estado_recepcion;
+        private int _id_orden_compra;
+        private string _rut_administrador;
+
+        public string Id_recepcion_compra
+        {
+            get
+            {
+                return _id_recepcion_compra;
+            }
+
+            set
+            {
+                _id_recepcion_compra = value;
+            }
+        }
+
+        public DateTime Fecha
+        {
+            get
+            {
+                return _fecha;
+            }
+
+            set
+            {
+                _fecha = value;
+            }
+        }
+
+        public string Comentarios
+        {
+            get
+            {
+                return _comentarios;
+            }
+
+            set
+            {
+                _comentarios = value;
+            }
+        }
+
+        internal EstadoRecepcion Estado_recepcion
+        {
+            get
+            {
+                return _estado_recepcion;
+            }
+
+            set
+            {
+                _estado_recepcion = value;
+            }
+        }
+
+        public int Id_estado_recepcion
+        {
+            get
+            {
+                return _id_estado_recepcion;
+            }
+
+            set
+            {
+                _id_estado_recepcion = value;
+            }
+        }
+
+        public int Id_orden_compra
+        {
+            get
+            {
+                return _id_orden_compra;
+            }
+
+            set
+            {
+                _id_orden_compra = value;
+            }
+        }
+
+        public string Rut_administrador
+        {
+            get
+            {
+                return _rut_administrador;
+            }
+
+            set
+            {
+                _rut_administrador = value;
+            }
+        }
+    }
+}
